dc85dcdadf64441a40644886745924571639d812
root <root@asuri-HP-Z400-Workstation.(none)>
2016-12-05 17:41:46 -0800

    Added caps filter element

--- newmp4.c
+++ newmp4.c
@@ -1,15 +1,3 @@
-/* 
-To compile 
-
-Build on : ubuntu 12.04 32 bit
-gstreamer version installed: 1.45
-
-gcc -Wall newmp4.c -o newmp4 -pthread -I/usr/local/include/gstreamer-1.0 -I/usr/include/glib-2.0 -I/usr/lib/i386-linux-gnu/glib-2.0/include  -L/usr/local/lib -lgstreamer-1.0 -lgobject-2.0 -lglib-2.0  
-
-Builds Pipeline: 
-gst-launch-1.0 -e filesrc location=/home/asuri/my_gstcode/SampleVideo_720x480_1mb.mp4 ! qtdemux name=mdemux mdemux.video_0 ! queue ! h264parse ! avdec_h264 ! videoconvert ! ximagesink mdemux.audio_0 ! queue ! faad ! audioconvert ! autoaudiosink
-
-*/
 #include <gst/gst.h>
 
 typedef struct _CustomData {
@@ -18,6 +6,7 @@ typedef struct _CustomData {
     GstElement *demuxer;
     GstElement *audioqueue;
     GstElement *videoqueue;
+    GstElement *avc_parser;
     GstElement *audio_decoder;
     GstElement *video_decoder;
     GstElement *video_convert;
@@ -32,6 +21,8 @@ static void pad_added_handler (GstElement *src, GstPad *pad, CustomData *data);
 
 int main(int argc, char *argv[]) 
 {
+    GstStructure *struc;
+    GstCaps *caps;
     GMainLoop *loop;
     CustomData data;
     GstBus *bus;
@@ -49,23 +40,41 @@ int main(int argc, char *argv[])
     data.audio_decoder = gst_element_factory_make ("faad", "audio_decoder");
     data.audio_convert = gst_element_factory_make ("audioconvert", "audio_convert");
     data.audio_sink = gst_element_factory_make ("autoaudiosink", "audio_sink");
+    data.avc_parser = gst_element_factory_make ("h264parse", "avc_parser");
     data.videoqueue = gst_element_factory_make("queue","videoqueue");
     data.video_decoder = gst_element_factory_make("avdec_h264","video_decoder");
     data.video_convert = gst_element_factory_make("videoconvert","video_convert");
     data.video_sink = gst_element_factory_make("ximagesink","video_sink");
     data.pipeline = gst_pipeline_new ("test-pipeline");
 
+#if 1
+     // Works for "nal"  but get an runtime warning 
+    /* Specify what kind of video is wanted from the camera  nal or au are options */
+    caps = gst_caps_new_simple( "video/x-h264, parsed=(boolean)false",
+     			        "stream-format", G_TYPE_STRING, "byte-stream",
+ 		                "alignment", G_TYPE_STRING, "nal",
+    			        NULL );
+#else
+     // Works for "AU" and not for NAL
+     caps = gst_caps_new_empty();
+     struc = gst_structure_new( "video/x-h264", 
+                                "alignment", G_TYPE_STRING, "au", 
+                                NULL );
+     gst_caps_append_structure(caps, struc);
+#endif 
+
+
     bus = gst_pipeline_get_bus (GST_PIPELINE (data.pipeline));
     gst_bus_add_watch (bus, my_bus_callback, loop);
     gst_object_unref (bus);
 
     if (!data.pipeline || !data.source || !data.demuxer || !data.audioqueue ||!data.audio_decoder ||!data.audio_convert ||
-    !data.audio_sink || !data.videoqueue || !data.video_decoder || !data.video_convert || !data.video_sink) {
+    !data.audio_sink || !data.videoqueue || !data.video_decoder || !data.video_convert || !data.video_sink || !data.avc_parser) {
     g_printerr ("Not all elements could be created.\n");
     return -1;
     }
 
-    gst_bin_add_many (GST_BIN (data.pipeline), data.source,data.demuxer,data.audioqueue,data.audio_decoder,data.audio_convert,data.audio_sink,data.videoqueue,data.video_decoder,data.video_convert,data.video_sink, NULL);
+    gst_bin_add_many (GST_BIN (data.pipeline), data.source,data.demuxer,data.audioqueue,data.audio_decoder,data.audio_convert,data.audio_sink,data.videoqueue,data.avc_parser, data.video_decoder,data.video_convert,data.video_sink, NULL);
 
     if (!gst_element_link(data.source,data.demuxer)) {
     g_printerr ("Elements could not be linked.\n");
@@ -78,8 +87,23 @@ int main(int argc, char *argv[])
     gst_object_unref (data.pipeline);
     return -1;
     }
+  
+    if (!gst_element_link_many(data.videoqueue, data.avc_parser, NULL)) {
+          g_printerr("video Elements could not be linked.\n");
+         gst_object_unref(data.pipeline);
+        return -1;
+    }
+   
+    if ( ! gst_element_link_filtered(data.avc_parser, data.video_decoder, caps) )
+    {
+       g_printerr (" Failed to link with filetered.\n");
+       gst_object_unref (data.pipeline);
+       gst_caps_unref(caps);
+       return -1;
+ 
+    }
 
-    if (!gst_element_link_many(data.videoqueue,data.video_decoder,data.video_convert, data.video_sink,NULL)) {
+    if (!gst_element_link_many( data.video_decoder, data.video_convert, data.video_sink,NULL)) {
     g_printerr("video Elements could not be linked.\n");
     gst_object_unref(data.pipeline);
     return -1;
@@ -101,9 +125,57 @@ int main(int argc, char *argv[])
     return -1;
     }
     gst_object_unref (bus);
+    gst_caps_unref(caps);
     gst_element_set_state (data.pipeline, GST_STATE_NULL);
     gst_object_unref (data.pipeline);
    
+#if 0 
+    
+    /* Listen to the bus */
+    bus = gst_element_get_bus (data.pipeline);
+    do {
+    msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
+    GST_MESSAGE_STATE_CHANGED | GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+    if (msg != NULL) {
+    GError *err;
+    gchar *debug_info;
+    switch (GST_MESSAGE_TYPE (msg)) 
+    {
+    case GST_MESSAGE_ERROR:
+    gst_message_parse_error (msg, &err, &debug_info);
+    g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+    g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
+    g_clear_error (&err);
+    g_free (debug_info);
+    terminate = TRUE;
+    break;
+    case GST_MESSAGE_EOS:
+    g_print ("End-Of-Stream reached.\n");
+    terminate = TRUE;
+    break;
+    case GST_MESSAGE_STATE_CHANGED:
+
+    if (GST_MESSAGE_SRC (msg) == GST_OBJECT (data.pipeline)) {
+    GstState old_state, new_state, pending_state;
+    gst_message_parse_state_changed (msg, &old_state, &new_state, &pending_state);
+    g_print ("Pipeline state changed from %s to %s:\n",
+    gst_element_state_get_name (old_state), gst_element_state_get_name (new_state));
+    }
+    break;
+    default:
+
+    g_printerr ("Unexpected message received.\n");
+    break;
+    }
+    gst_message_unref (msg);
+    }
+    } while (!terminate); 
+
+    gst_object_unref (bus);
+    gst_element_set_state (data.pipeline, GST_STATE_NULL);
+    gst_object_unref (data.pipeline);
+#endif 
     return 0;
     }
 
@@ -111,6 +183,7 @@ int main(int argc, char *argv[])
 static void pad_added_handler (GstElement *src, GstPad *new_pad, CustomData *data) 
 {
     GstPad *sink_pad_audio = gst_element_get_static_pad (data->audioqueue, "sink");
+    
     GstPad *sink_pad_video = gst_element_get_static_pad (data->videoqueue, "sink");
 
     GstCaps *new_pad_caps;
